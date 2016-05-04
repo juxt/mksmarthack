af43ad66a7266cdce769df8abacabfd60bed56e6
Malcolm Sparks <malcolm@juxt.pro>
2016-05-04 21:52:22 +0100

    Add translation between OSGB and latlng

--- src/mksmarthack/core.clj
+++ src/mksmarthack/core.clj
@@ -12,9 +12,59 @@
    [yada.yada :as yada]
    ))
 
+(defn osgb->latlng [eastings northings]
+  (let [crsFac
+        (org.geotools.referencing.ReferencingFactoryFinder/getCRSAuthorityFactory "EPSG" nil)
+        wgs84crs (.createCoordinateReferenceSystem crsFac "4326")
+        osgbCrs (.createCoordinateReferenceSystem crsFac "27700")
+        op (.createOperation (new org.geotools.referencing.operation.DefaultCoordinateOperationFactory)
+                             osgbCrs wgs84crs)
+        pos (new org.geotools.geometry.GeneralDirectPosition eastings northings)
+        latlng (.. op getMathTransform (transform pos pos))]
+
+    [(.getOrdinate latlng 0) (.getOrdinate latlng 1)]
+    ))
+
+;; (osgb->latlng 488917 251298)
+
+
+(defn add-latlng [toilet]
+  (assoc toilet
+         "LatLng"
+         (osgb->latlng
+          (Long/parseLong (get toilet "GeoX"))
+          (Long/parseLong (get toilet "GeoY")))))
+
+(defn get-toilets []
+  (let [data (csv/read-csv
+              (slurp "http://www.milton-keynes.gov.uk/assets/attach/21191/Public%20Toilets.csv"))
+        header (first data)
+        rows (rest data)]
+    (map add-latlng
+     (map #(zipmap header %) rows))))
+
 (defn routes []
   ["/"
    [
+    ["toilets" (yada/resource
+                {:methods
+                 {:get
+                  {:produces #{"application/json"
+                               "application/edn"
+                               "application/transit+json"
+                               "application/transit+msgpack"
+                               "text/csv"}
+                   :response (fn [ctx]
+                               (case (yada/content-type ctx)
+                                 "text/csv"
+                                 (slurp "http://www.milton-keynes.gov.uk/assets/attach/21191/Public%20Toilets.csv")
+                                 ("application/json" "application/edn")
+                                 (get-toilets)
+
+
+                                 )
+                               )}}}
+                )]
     ["index.html" (yada/resource
                    {:id ::index
                     :methods
